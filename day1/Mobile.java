6358dfb43794b86cb159694defddfc9b69292892
Sathiyapriya-238 <121354079+Sathiyapriya-238@users.noreply.github.com>
2022-12-25 13:50:22 +0530

    Update Mobile.java

--- day1/Mobile.java
+++ day1/Mobile.java
@@ -3,10 +3,12 @@ package week1.day1;
 public class Mobile {
 public  void makeCall(String mobileName,float mobileWeight) {
 	System.out.println("Call is connecting");
-	System.out.println(mobileName + mobileWeight);
+	System.out.println(mobileName);
+	System.out.println(mobileWeight);
 	}
 public  void sendMsg(boolean isFullCharged,int mobileCost) {
 	System.out.println("Message is sent");
+	System.out.println(isFullCharged);
 	System.out.println(mobileCost);
 }
 public  static void  main(String[] args) {
